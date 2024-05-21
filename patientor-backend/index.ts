b857d69cbf6f992a5c6a855847eda1c9a86f8e58
Sushant Bajracharya <bajra.sushant@gmail.com>
2024-05-21 13:25:55 +0545

    setup:patientor app

---
+++ patientor-backend/index.ts
@@ -0,0 +1,18 @@
+import express from "express";
+import cors = require("cors");
+
+const app = express();
+app.use(express.json());
+
+app.use(cors());
+
+const PORT = 3001;
+
+app.get("/api/ping", (_req, res) => {
+  console.log("pinged");
+  res.send("pong");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening at port ${PORT}`);
+});
