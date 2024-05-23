32d0a30e28ff2f514f63fb90c0ae86d22964bdfd
Sushant Bajracharya <bajra.sushant@gmail.com>
2024-05-23 10:53:32 +0545

    9.13 complete
    typing an express app
    post request to add patients
    request data parsing to make sure we get the type of data we are
    expecting always

--- patientor-backend/src/routes/patient.ts
+++ patientor-backend/src/routes/patient.ts
@@ -1,10 +1,26 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import express from "express";
 import patientService from "../services/patientService";
+import toNewPatientEntry from "../utils";
 
 const router = express.Router();
 
 router.get("/", (_req, res) => {
   res.send(patientService.getPatientsNoSsn());
+});
+
+router.post("/", (req, res) => {
+  try {
+    const newPatientEntry = toNewPatientEntry(req.body);
+    const addedPatient = patientService.addNewPatient(newPatientEntry);
+    res.json(addedPatient);
+  } catch (error: unknown) {
+    let errorMessage = "Something went wrong.";
+    if (error instanceof Error) {
+      errorMessage += " Error: " + errorMessage;
+    }
+    res.status(400).send(errorMessage);
+  }
 });
 
 export default router;
--- patientor-backend/src/services/patientService.ts
+++ patientor-backend/src/services/patientService.ts
@@ -1,7 +1,8 @@
 import patientData from "../../data/patients";
-import { Patient, ssnExcludedPatient } from "../types";
+import { NewPatientEntry, Patient, ssnExcludedPatient } from "../types";
+import { v1 as uuid } from "uuid";
 
-const getPatients = ():Patient[] => {
+const getPatients = (): Patient[] => {
   return patientData;
 };
 
@@ -15,7 +16,17 @@ const getPatientsNoSsn = (): ssnExcludedPatient[] => {
   }));
 };
 
+const addNewPatient = (entry: NewPatientEntry): Patient => {
+  const newPatient = {
+    id: uuid(),
+    ...entry,
+  };
+  patientData.push(newPatient);
+  return newPatient;
+};
+
 export default {
   getPatients,
   getPatientsNoSsn,
+  addNewPatient
 };
